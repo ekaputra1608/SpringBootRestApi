c18d6354ebd9869e1736668fc0077ba923685020
ekaputra1608 <ekaputra1608@gmail.com>
2017-09-14 17:05:17 +0700

    add new service

--- src/main/java/com/mr/controller/HomeController.java
+++ src/main/java/com/mr/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.mr.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -13,6 +14,13 @@ public class HomeController {
     public Map welcome() {
         Map map = new HashMap();
         map.put("message", "welcome");
+        return map;
+    }
+    
+    @GetMapping("/checkSession/{sessionId}")
+    public Map checkSessionId(@PathVariable String sessionId) {
+        Map map = new HashMap();
+        map.put("status", sessionId.equals("1") ? "valid" : "invalid");
         return map;
     }
 }
